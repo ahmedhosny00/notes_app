36744387981aeae18dd5cf91956fa5889a9d3366
Ahmed_Hosny <ahmed.hosny.roshdy@gmail.com>
2024-06-21 18:41:57 +0300

    create edit note view

--- lib/views/wedgits/custom_note_item.dart
+++ lib/views/wedgits/custom_note_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/wedgits/edit_note_view_body.dart';
 
 class NotesItem extends StatelessWidget {
   const NotesItem({super.key});
@@ -9,56 +10,68 @@ class NotesItem extends StatelessWidget {
       padding: const EdgeInsets.symmetric(
         horizontal: 16,
       ),
-      child: Container(
-        padding: const EdgeInsets.only(
-          top: 24,
-          bottom: 24,
-          left: 16,
-        ),
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: const Color(0xFFffcd7a),
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: const Text(
-                'Flutter Note',
-                style: TextStyle(
-                  fontSize: 26,
-                  color: Colors.black,
-                ),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 16.0),
-                child: Text(
-                  'Build a notes app with flutters',
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return const EditNoteViewBody();
+              },
+            ),
+          );
+        },
+        child: Container(
+          padding: const EdgeInsets.only(
+            top: 24,
+            bottom: 24,
+            left: 16,
+          ),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: const Color(0xFFffcd7a),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: const Text(
+                  'Flutter Note',
                   style: TextStyle(
-                      fontSize: 18, color: Colors.black.withOpacity(0.5)),
+                    fontSize: 26,
+                    color: Colors.black,
+                  ),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: Text(
+                    'Build a notes app with flutters',
+                    style: TextStyle(
+                        fontSize: 18, color: Colors.black.withOpacity(0.5)),
+                  ),
+                ),
+                trailing: IconButton(
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.black,
+                    size: 28,
+                  ),
+                  onPressed: () {},
                 ),
               ),
-              trailing: IconButton(
-                icon: const Icon(
-                  Icons.delete,
-                  color: Colors.black,
-                  size: 28,
-                ),
-                onPressed: () {},
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 16.0, top: 8.0),
-              child: Text(
-                'May 19 , 2024 ',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black.withOpacity(0.5),
+              Padding(
+                padding: const EdgeInsets.only(right: 16.0, top: 8.0),
+                child: Text(
+                  'May 19 , 2024 ',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black.withOpacity(0.5),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
